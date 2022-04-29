17c8b3c86768f2a6f3711a3d9422d531a9e9840b
assraf-nur <assraf.nur@gmail.com>
2022-04-30 01:14:49 +0600

    added firebase authentication in login and register

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,14 +1,42 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Login = () => {
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+
+    const navigate = useNavigate();
+
+    const navigateRegister = () =>{
+        navigate('/register')
+    }
+
+    const handleLogin  = e =>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        signInWithEmailAndPassword(email, password)
+        
+    }
+
+    if(user){
+        navigate('/home')
+    }
+
   return (
-    <Form className="container mx-auto rounded-2xl shadow-2xl border mt-5 w-50 mb-5 p-4">
+    <Form onSubmit={handleLogin} className="container mx-auto rounded-2xl shadow-2xl border mt-5 w-50 mb-5 p-4">
         <h2 className="text-center font-bold">Login</h2>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" required/>
+        <Form.Control type="email" name="email" placeholder="Enter email" required/>
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -16,7 +44,7 @@ const Login = () => {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" required/>
+        <Form.Control type="password" name="password" placeholder="Password" required/>
         <Form.Text className="text-muted">
           We'll never share your password with anyone else.
         </Form.Text>
@@ -24,7 +52,14 @@ const Login = () => {
       <Button className="px-5" variant="primary" type="submit">
         Login
       </Button>
-      <p className="mt-3">New to site ? <Link to='/register' className="text-danger text-decoration-none">Register Now</Link> </p>
+      <p className="mt-3">New to site ? <Link to='/register' onClick={navigateRegister} className="text-danger text-decoration-none">Register Now</Link> </p>
+      <br />
+      {
+          error && <p>User and password not found</p>
+      }
+      {
+          loading && <p>Loading.....</p>
+      }
     </Form>
   );
 };
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,33 +1,65 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
+    const navigate = useNavigate();
+
+    const navigateRegister = () =>{
+        navigate('/login')
+    }
+
+    const handleRegister = e =>{
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        createUserWithEmailAndPassword(email, password);
+    }
+    if(user){
+        navigate('/home')
+    }
+
   return (
-    <Form className="w-50 p-4 mx-auto border rounded-2xl shadow mt-5">
+    <Form onSubmit={handleRegister} className="w-50 p-4 mx-auto border rounded-2xl shadow mt-5">
         <h2 className="text-center font-bold">Registration</h2>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Name" required/>
+        <Form.Control type="text" name="name" placeholder="Name" required/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Email" required/>
+        <Form.Control type="email" name="email" placeholder="Email" required/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" required/>
+        <Form.Control type="password" name="password" placeholder="Password" required/>
       </Form.Group>
 
       <Button className="mx-auto px-5" variant="primary" type="submit">
         Register
       </Button>
 
-      <p className="mt-3">Already have an account ? <Link to='/login' className="text-danger text-decoration-none">Login</Link> </p>
-
+      <p className="mt-3">Already have an account ? <Link to='/login' onClick={navigateRegister} className="text-danger text-decoration-none">Login</Link> </p>
+        {
+            error && <p>Please give valid mail and password</p>
+        }
+        {
+            loading && <p>Loading......</p>
+        }
     </Form>
   );
 };
