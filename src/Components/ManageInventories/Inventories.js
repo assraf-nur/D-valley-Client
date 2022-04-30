aa7ede043dd09d3274528e77b2c989c88f3f463b
assraf-nur <assraf.nur@gmail.com>
2022-05-01 00:47:12 +0600

    added a delete api

--- src/Components/ManageInventories/Inventories.js
+++ src/Components/ManageInventories/Inventories.js
@@ -2,7 +2,22 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 
 const Inventories = ({product}) => {
-    const {_id, name, price, img, des, quantity, supplier } = product;
+    const {_id, name, img, quantity, supplier } = product;
+
+    const handleDeleteProduct  = ({id, e}) =>{
+      const proceed = window.confirm("Are you really want to delete ?");
+      if(proceed){
+        const url = `http://localhost:5000/products/${id}`
+        fetch(url, {
+          method: 'DELETE',
+        })
+        .then(res => res.json())
+        .then(data =>{
+           
+        })
+      }
+    }
+
     return (
         <Card className="mx-auto shadow" style={{ width: '18rem' }}>
         <Card.Img className="p-3 w-50 mx-auto" variant="top" src={img} />
@@ -12,7 +27,7 @@ const Inventories = ({product}) => {
          <span className="fw-bold">Supplier: {supplier}</span> <br /> <span>Quantity: {quantity}</span>
           </Card.Text>
         </Card.Body>
-        <button className="btn btn-danger w-100 mt-2 mb-2">Delete</button>
+        <button onClick={()=> handleDeleteProduct(_id)} className="btn btn-danger w-100 mt-2 mb-2">Delete</button>
       </Card>
     );
 };
--- src/Components/ManageInventories/ManageInventories.js
+++ src/Components/ManageInventories/ManageInventories.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
 import Inventories from "./Inventories";
 
 const ManageInventories = () => {
@@ -10,14 +11,50 @@ const ManageInventories = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  const handleDeleteProduct  = id =>{
+    const proceed = window.confirm("Are you really want to delete ?");
+    if(proceed){
+      const url = `http://localhost:5000/products/${id}`;
+      fetch(url, {
+        method: 'DELETE',
+      })
+      .then(res => res.json())
+      .then(data =>{
+         if(data.deletedCount > 0){
+           const remaining = products.filter(product => product._id !== id);
+           setProducts(remaining);
+         }
+      })
+    }
+  }
+
   return (
     <div className="container mt-3">
-      <h2 data-aos="fade-down" className="text-center mb-3 fw-bold"> Manage Inventories </h2>
+      <h2 data-aos="fade-down" className="text-center mb-3 fw-bold">
+        {" "}
+        Manage Inventories{" "}
+      </h2>
       <div data-aos="fade-down-left" className="row container gap-5">
-      {
-      products.map((product) => (
-              <Inventories key={product._id} product={product}></Inventories>
-            ))};
+        {products.map((product) => (
+          <Card className="mx-auto shadow" style={{ width: "18rem" }}>
+            <Card.Img className="p-3 w-50 mx-auto" variant="top" src={product.img} />
+            <Card.Body className="border rounded shadow-sm">
+              <Card.Title>{product.name}</Card.Title>
+              <Card.Text>
+                <span className="fw-bold">Supplier: {product.supplier}</span> <br />{" "}
+                <span>Quantity: {product.quantity}</span>
+              </Card.Text>
+            </Card.Body>
+            <button
+              onClick={() => handleDeleteProduct(product._id)}
+              className="btn btn-danger w-100 mt-2 mb-2"
+            >
+              Delete
+            </button>
+          </Card>
+        ))}
+        ;
       </div>
     </div>
   );
